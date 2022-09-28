11be02460aaa6e4daa37755631ec1be0ded462eb
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2022-09-27 21:35:07 -0500

    Add file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
