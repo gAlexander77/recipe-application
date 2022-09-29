dd3969e98f9f350755b36252865b8898b971576d
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2022-09-29 00:39:35 -0500

    Added account page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Account from './pages/Account';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/account" element={<Account/>}/>
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/Account.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Account() {
+    return(
+        <div className="Account">
+            <h1>Header</h1>
+            <h1>Account Info</h1>
+            <h1>My Recipes</h1>
+            <h1>Saved Recipes</h1>
+        </div>
+    );
+}
+
+export default Account;
