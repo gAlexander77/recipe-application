de7970190460a2d8d4c3ea5584adcce9e996fe58
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2022-10-12 20:17:29 -0500

    Add individual recipe page

---
+++ frontend/src/pages/Recipe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Recipe(){
+    return(
+        <div className="Recipe">
+            <h1>Image</h1>
+            <h1>Recipe Name</h1>
+            <h1>Recipe Rating</h1>
+            <h1>Recipe Description</h1>
+            <h1>Recipe Ingredient</h1>
+            <h1>Recipe Directions</h1>
+            <button>Download Recipe</button>
+            <button>Rate Recipe</button>
+        </div>
+    );   
+}
+
+export default Recipe;
