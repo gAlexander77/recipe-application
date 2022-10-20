7e139260daf65119595bc53ad4b9311eca5f8700
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2022-10-19 23:51:54 -0500

    Add logout

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import NavigationBar from '../components/NavigationBar';
 import '../styles/AccountStyle.css';
 
@@ -8,6 +9,8 @@ function Account() {
     let navigate = useRef(useNavigate());
     const [userType, setUserType] = useState(localStorage.getItem('userType'));
 
+    const [username, setUsername] = useState('username');
+
     useEffect(()=>{ 
         setUserType(localStorage.getItem('userType'));
         if (userType !== 'user'){
@@ -15,28 +18,59 @@ function Account() {
         }
     },[userType])
 
+    const [recipes, setRecipes] = useState([])
+    /* Test Mock API */
+    useEffect(()=>{
+        axios.get('https://f3fad6f8-6516-4627-a8dd-ac467a4107cf.mock.pstmn.io/recipes').then(res => {
+        setRecipes(res.data)
+        }).catch(error => alert('API ERROR'));
+    }, []);
+
+    const logout = () => {
+        localStorage.removeItem('userType');
+        localStorage.removeItem('userID');
+        localStorage.removeItem('username');
+        setUserType(localStorage.getItem('userType'));        
+    }
+
+    function AccountRecipeCard({image, name, id}) {
+        return (
+            <div>
+                <div>DELETE</div>
+                <img src={image} className="box">
+                </img>
+                <p>{name} {id}</p>
+            </div>
+        );
+    }
+
     return(
         <div className="Account">
             <div className="navbar">
                 <NavigationBar {...{userType}}/>
             </div>
+            
             <div className="account-information">
-                <div className="circle"></div>
-                <h1 className="account-username">Username</h1>
-                <button className="btn">Settings</button>
+                <div className="circle">
+                </div>
+                <h1 className="account-username">{username}</h1>
+                <button className="btn" onClick={logout}>Logout</button>
             </div>
+
             <div className="saved-recipes-container">
                 <div className="row-container">
                     <h1 className="row-title">Saved Recipes</h1>
                     <div className="recipe-row">
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
+                        {recipes.map((recipe,index) =>{
+                            return(
+                                <AccountRecipeCard
+                                key = {index}
+                                id = {recipe.id}
+                                image={recipe.image} 
+                                name={recipe.recipe_name} 
+                                />
+                            );
+                        })}
                     </div>
                 </div>
             </div>
