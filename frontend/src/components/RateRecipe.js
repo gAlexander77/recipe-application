6f148cc78e2648c14faa3c356893b069872b2524
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2022-10-20 02:53:41 -0500

    Add recipe rating

--- frontend/src/components/RateRecipe.js
+++ frontend/src/components/RateRecipe.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState}from 'react';
 import { BsXLg } from "react-icons/bs";
+import '../styles/components/RateRecipeStyle.css';
 
 function RateRecipe(props){
+    const [rating, setRating] = useState(0);
+
+    const rateRecipe = () => {
+        console.log(rating);
+        props.setTrigger(false);
+    }
+
     return(props.trigger)?(
         <div className="rate-recipe-popup">
             <div className="rate-recipe-inner">
                 <BsXLg className="exit-rate-recipe" onClick={() => props.setTrigger(false)}/>
                 <h1>Rate Recipe</h1>
+                <div>
+                    <button onClick={()=>setRating(1)}>1</button>
+                    <button onClick={()=>setRating(2)}>2</button>
+                    <button onClick={()=>setRating(3)}>3</button>
+                    <button onClick={()=>setRating(4)}>4</button>
+                    <button onClick={()=>setRating(5)}>5</button>
+                    <div>{rating}/5 Stars</div>
+                </div>
+                <button onClick={rateRecipe}>Submit Rating</button>
             </div>
         </div>
     ):"";
