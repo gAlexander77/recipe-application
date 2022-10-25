b7cbf3ba2fe1d8e80319db7cd5089db54f335a42
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2022-10-25 09:42:51 -0500

    Add delete

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
+import { BsXCircle, BsCaretLeftSquareFill, BsCaretRightSquareFill } from "react-icons/bs";
+import { TbTrash } from "react-icons/tb";
 import NavigationBar from '../components/NavigationBar';
 import '../styles/AccountStyle.css';
 
@@ -10,7 +12,6 @@ function Account() {
 
     let navigate = useRef(useNavigate());
     const [userType, setUserType] = useState(localStorage.getItem('userType'));
-
     const [username, setUsername] = useState('username');
 
     useEffect(()=>{ 
@@ -35,13 +36,66 @@ function Account() {
         setUserType(localStorage.getItem('userType'));        
     }
 
+    
+    const [isDeleting, setIsDeleting] = useState(false);
+
     function AccountRecipeCard({image, name, id}) {
+
+        const deleteRecipeHandler = () => {
+            setRecipes(recipes.filter(recipe=>recipe.id !== id));
+        }
+
         return (
             <div>
-                <div>DELETE</div>
-                <img src={image} className="box">
-                </img>
-                <p>{name} {id}</p>
+                <BsXCircle className={`${isDeleting ? "delete-recipe-btn ": "hidden"}`} onClick={deleteRecipeHandler}/>
+                <BsXCircle className={`${isDeleting ? "hidden": "hide-delete-recipe-btn"}`}/>
+                <div className="box-container">
+                    <Link to={"/recipe/"+id} state={id} onClick={() => console.log("clicked "+id)}> 
+                        <img src={image} className="box"></img>
+                    </Link>
+                    <p className="my-recipe-name">{name}</p>
+                </div>
+            </div>
+        );
+    }
+
+    function MyRecipes() {
+        return(
+            <div className="my-recipes-container">
+                <div className="row-container">
+                    <div className="row-header">
+                        <h1 className="row-title">My Recipes</h1>
+                        <TbTrash className={`${isDeleting ? "trash-icon-active": "trash-icon"}`} onClick={()=> setIsDeleting(!isDeleting)}/>
+                    </div>
+                    <div className="recipe-row-container">
+                        <BsCaretLeftSquareFill className="left-btn" id="slideRight"/>
+                        <div className="recipe-row" id="container">    
+                            {recipes.map((recipe,index) =>{
+                                return(
+                                    <AccountRecipeCard
+                                    key = {index}
+                                    id = {recipe.id}
+                                    image={recipe.image} 
+                                    name={recipe.recipe_name} 
+                                    />
+                                );
+                            })}    
+                        </div>
+                        <BsCaretRightSquareFill className="right-btn" id="slideLeft"/>
+                    </div>    
+                </div>
+            </div>
+        );
+    }
+
+    function AccountInfo() {
+        return(
+            <div className="account-information">
+                <div className="circle">
+                    <h1 className="letter">{username[0].toUpperCase()}</h1>
+                </div>
+                <h1 className="account-username">@{username}</h1>
+                <button className="logout-btn" onClick={logout}>Logout</button>
             </div>
         );
     }
@@ -51,46 +105,8 @@ function Account() {
             <div className="navbar">
                 <NavigationBar {...{userType}}/>
             </div>
-            
-            <div className="account-information">
-                <div className="circle">
-                </div>
-                <h1 className="account-username">{username}</h1>
-                <button className="btn" onClick={logout}>Logout</button>
-            </div>
-
-            <div className="saved-recipes-container">
-                <div className="row-container">
-                    <h1 className="row-title">Saved Recipes</h1>
-                    <div className="recipe-row">
-                        {recipes.map((recipe,index) =>{
-                            return(
-                                <AccountRecipeCard
-                                key = {index}
-                                id = {recipe.id}
-                                image={recipe.image} 
-                                name={recipe.recipe_name} 
-                                />
-                            );
-                        })}
-                    </div>
-                </div>
-            </div>
-            <div className="my-recipes-container">
-                <div className="row-container">
-                    <h1 className="row-title">My Recipes</h1>
-                    <div className="recipe-row">
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
-                        <div className="box"></div>
-                    </div>
-                </div>
-            </div>
+            <AccountInfo/>
+            <MyRecipes/>
         </div>
     );
 }
