80785a48c7ecab027cdb5455a96c7dd67d59e090
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2022-10-29 23:34:27 -0500

    Update style

--- frontend/src/components/RateRecipe.js
+++ frontend/src/components/RateRecipe.js
@@ -1,4 +1,5 @@
 import React, {useState}from 'react';
+import { motion } from 'framer-motion';
 import { BsXLg } from "react-icons/bs";
 import '../styles/components/RateRecipeStyle.css';
 
@@ -12,19 +13,24 @@ function RateRecipe(props){
 
     return(props.trigger)?(
         <div className="rate-recipe-popup">
-            <div className="rate-recipe-inner">
+            <motion.div className="rate-recipe-inner"
+            initial={{ opacity: 0, y: "300px" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ opacity: 0.2, y: 0.3 }}
+            >
                 <BsXLg className="exit-rate-recipe" onClick={() => props.setTrigger(false)}/>
                 <h1>Rate Recipe</h1>
                 <div>
-                    <button onClick={()=>setRating(1)}>1</button>
-                    <button onClick={()=>setRating(2)}>2</button>
-                    <button onClick={()=>setRating(3)}>3</button>
-                    <button onClick={()=>setRating(4)}>4</button>
-                    <button onClick={()=>setRating(5)}>5</button>
-                    <div>{rating}/5 Stars</div>
+                    <button className="number-btn" onClick={()=>setRating(1)}>1</button>
+                    <button className="number-btn" onClick={()=>setRating(2)}>2</button>
+                    <button className="number-btn" onClick={()=>setRating(3)}>3</button>
+                    <button className="number-btn" onClick={()=>setRating(4)}>4</button>
+                    <button className="number-btn" onClick={()=>setRating(5)}>5</button>    
                 </div>
-                <button onClick={rateRecipe}>Submit Rating</button>
-            </div>
+                <p>{rating}/5 Stars</p>
+                <button className="submit-rating-btn" onClick={rateRecipe}>Submit Rating</button>
+            </motion.div>
         </div>
     ):"";
 }
