3a786b88914027c9ff24a9c661fd1e1371372e5a
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2022-10-30 16:44:47 -0500

    Update account page

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import axios from 'axios';
 import { BsXCircle, BsCaretLeftSquareFill, BsCaretRightSquareFill } from "react-icons/bs";
 import { TbTrash } from "react-icons/tb";
@@ -45,13 +46,30 @@ function Account() {
             setRecipes(recipes.filter(recipe=>recipe.id !== id));
         }
 
+        const RecipeImage = () => {
+            if(isDeleting === true) {
+            return(
+                <motion.img src={image} className="box"
+                        animate={{
+                            rotate: [0,2,0,-2,0],
+                          }}
+                          transition={{
+                            duration: 0.25,
+                            repeat: Infinity
+                          }}/>
+            );}
+            else
+            return(<img src={image} className="box"/>);
+        };
+
+
         return (
             <div>
                 <BsXCircle className={`${isDeleting ? "delete-recipe-btn ": "hidden"}`} onClick={deleteRecipeHandler}/>
                 <BsXCircle className={`${isDeleting ? "hidden": "hide-delete-recipe-btn"}`}/>
                 <div className="box-container">
                     <Link to={"/recipe/"+id} state={id} onClick={() => console.log("clicked "+id)}> 
-                        <img src={image} className="box"></img>
+                        <RecipeImage/>
                     </Link>
                     <p className="my-recipe-name">{name}</p>
                 </div>
@@ -59,7 +77,33 @@ function Account() {
         );
     }
 
+    
+
     function MyRecipes() {
+        
+        const EmptyRow = () => {
+            if(recipes.length === 0)
+            return(
+                <div className="empty-row">
+                <p className="empty-text">You haven't posted any recipes</p>
+                </div>
+            );
+        };
+
+        const MapRecipes = () => {
+            
+        }
+        
+        const myRef = useRef();
+        
+        const slideLeft = () => {
+            myRef.current.scrollLeft += -500;
+        };
+
+        const slideRight = () => {
+            myRef.current.scrollLeft += 500;
+        };
+        
         return(
             <div className="my-recipes-container">
                 <div className="row-container">
@@ -68,8 +112,8 @@ function Account() {
                         <TbTrash className={`${isDeleting ? "trash-icon-active": "trash-icon"}`} onClick={()=> setIsDeleting(!isDeleting)}/>
                     </div>
                     <div className="recipe-row-container">
-                        <BsCaretLeftSquareFill className="left-btn" id="slideRight"/>
-                        <div className="recipe-row" id="container">    
+                        <BsCaretLeftSquareFill className="left-btn" onClick={slideLeft}/>
+                        <div className="recipe-row" id="container" ref={myRef}>    
                             {recipes.map((recipe,index) =>{
                                 return(
                                     <AccountRecipeCard
@@ -79,9 +123,10 @@ function Account() {
                                     name={recipe.recipe_name} 
                                     />
                                 );
-                            })}    
+                            })}
+                        <EmptyRow/>    
                         </div>
-                        <BsCaretRightSquareFill className="right-btn" id="slideLeft"/>
+                        <BsCaretRightSquareFill className="right-btn" onClick={slideRight}/>
                     </div>    
                 </div>
             </div>
@@ -105,8 +150,13 @@ function Account() {
             <div className="navbar">
                 <NavigationBar {...{userType}}/>
             </div>
-            <AccountInfo/>
-            <MyRecipes/>
+            <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            >
+                <AccountInfo/>
+                <MyRecipes/>
+            </motion.div>
         </div>
     );
 }
