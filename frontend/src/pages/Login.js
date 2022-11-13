33d5459a6f9f634860bd4f1d38d909c415265b8e
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2022-11-13 00:57:03 -0600

    Update login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import '../styles/LoginStyle.css'
 import { Link } from "react-router-dom";
 import { FaUser,FaLock } from "react-icons/fa"
@@ -13,22 +13,78 @@ function Login () {
     const [hasAccount,setHasAccount] = useState(null)
 
     function SignUp() {
+
+        const [data, setData] = useState({
+            username: "",
+            password: "",
+            verifyPassword: ""
+        });
+        
+        const [passwordVerified, setPasswordVerified] = useState(false);
+        useEffect(()=>{
+            if(data.password==="" || data.verifyPassword==="")
+                setPasswordVerified(null);
+            else if(data.password===data.verifyPassword)
+                setPasswordVerified(true);
+            else if(data.password!==data.verifyPassword)
+                setPasswordVerified(false);
+        },[data])
+
+        const changeValueHandler = (evt) => {
+            const newData={...data};
+            newData[evt.target.id] = evt.target.value;
+            setData(newData);
+        }
+
+        function DoPasswordsMatch() {
+            if(passwordVerified===true)
+                return(
+                    <div>Passwords Match</div>
+                );
+            else if(passwordVerified===false)
+                return(
+                    <div>Passwords Do Not Match</div>
+                );
+        }
+
         return (
             <form className="box-size center">
                 <BsXLg className="form-exit" onClick={()=>setHasAccount(null)}/>
                 <div className="content-box center">
                     <div className="input-row">
                         <FaUser className="input-logo"/>
-                        <input className="inpt Username" placeholder="Username" type="text"></input>
+                        <input 
+                            className="inpt Username" 
+                            placeholder="Username" 
+                            type="text"
+                            id="username" 
+                            value={data.username}
+                            onChange={(evt)=>changeValueHandler(evt)}
+                            />
                     </div>
                     <div className="input-row">
-                    <FaLock className="input-logo"/>
-                        <input className="inpt Password" placeholder="Password" type="password"></input>
+                        <FaLock className="input-logo"/>
+                        <input 
+                            className="inpt Password" 
+                            placeholder="Password" 
+                            type="password"
+                            id="password" 
+                            value={data.password}
+                            onChange={(evt)=>changeValueHandler(evt)}
+                            />
                         </div>
                     <div className="input-row">
                         <FaLock className="input-logo"/>
-                        <input className="inpt verify-password" placeholder="Verify Password" type="password"></input>
+                        <input 
+                            className="inpt verify-password" 
+                            placeholder="Verify Password" 
+                            type="password"
+                            id="verifyPassword" 
+                            value={data.verifyPassword}
+                            onChange={(evt)=>changeValueHandler(evt)}
+                            />
                     </div>
+                    <DoPasswordsMatch/>
                     <button className="btn-hover btn">Create Account</button>
                 </div>
             </form>
@@ -36,18 +92,43 @@ function Login () {
     }
 
     function SignIn() {
+
+        const [data, setData] = useState({
+            username: "",
+            password: ""
+        });
+
+        const changeValueHandler = (evt) => {
+            const newData={...data};
+            newData[evt.target.id] = evt.target.value;
+            setData(newData);
+        }
+
         return (
             <form className="box-size center">
                 <BsXLg className="form-exit" onClick={()=>setHasAccount(null)}/>
                 <div className="content-box center">
                     <div className="input-row">  
                         <FaUser className="input-logo"/>
-                        <input className="inpt" type="text" placeholder="Username"></input>
-
+                        <input 
+                            className="inpt" 
+                            type="text" 
+                            placeholder="Username" 
+                            id="username" 
+                            value={data.username}
+                            onChange={(evt)=>changeValueHandler(evt)}
+                            />
                     </div>
                     <div className="input-row">
                         <FaLock className="input-logo"/>
-                        <input className="inpt" type="password" placeholder="Password"></input>
+                        <input 
+                            className="inpt" 
+                            type="password" 
+                            placeholder="Password" 
+                            id="password" 
+                            value={data.password}
+                            onChange={(evt)=>changeValueHandler(evt)}
+                            />
                     </div>
                         <button className="btn-hover btn">Login</button>
                 </div>
@@ -61,7 +142,7 @@ function Login () {
                 <button className="btn-hover btn" onClick={() => setHasAccount(true)}>Login</button>
                 <button className="btn-hover btn" onClick={() => setHasAccount(false)}>Create Account</button>    
                 <Link to="/" onClick={guestHandler}>
-                Continue as a guest
+                    Continue as a guest
                 </Link>
             </div>
         );
